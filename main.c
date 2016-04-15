0a354b9f0fbf833bb230135e3d7424657b48ae3b
Hugues PONCET <hponcet@e3r9p10.42.fr>
2016-04-15 20:58:51 +0200

    gitp

--- main.c
+++ main.c
@@ -6,35 +6,21 @@
 /*   By: hponcet <hponcet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/15 15:02:50 by hponcet           #+#    #+#             */
-/*   Updated: 2016/04/15 15:19:18 by hponcet          ###   ########.fr       */
+/*   Updated: 2016/04/15 20:58:42 by hponcet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <curses.h>
+#include <termios.h>
+#include <unistd.h>
 #include <term.h>
-#include "ft_select.h"
+#include <curses.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include "libft/includes/libft.h"
 
-int              main(int ac, char **av, char **env)
-{
-	char           *name_term;
-	struct termios *term;
-
-	ac = 0;
-	av = NULL;
-	term = NULL;
-	env = NULL;
-	if ((name_term = getenv("TERM")) == NULL)
-		return (-1);
-	if (tgetent(NULL, name_term) == -1)
-		return (-1);
-	// remplis la structure termios des possibilités du terminal.
-	if (tcgetattr(0, term) == -1)
-		return (-1);
-	voir_touche();
-	return (0);
-}
-
-int     voir_touche()
+static int     voir_touche()
 {
 	char     buffer[3];
 
@@ -42,12 +28,34 @@ int     voir_touche()
 	{
 		read(0, buffer, 3);
 		if (buffer[0] == 27)
-			ft_printf("C'est une fleche !\n");
+			printf("C'est une fleche !\n");
 		else if (buffer[0] == 4)
-		{
-			ft_printf("Ctlr+d, on quitte !\n");
-			return (0);
-		}
+			printf("Ctlr+d, on quitte !\n");
 	}
 	return (0);
 }
+
+int              main(int ac, char **av, char **env)
+{
+	char           *name_term;
+	struct termios term;
+
+	ac = 0;
+	av = NULL;
+	name_term = ft_strdup("xterm-256color");
+	if (tgetent(NULL, name_term) == ERR)
+	{
+		printf("51\n");
+		return (-1);
+	}
+	// remplis la structure termios des possibilités du terminal.
+	if (tcgetattr(0, &term) == -1)
+	{
+		printf("55\n");
+		return (-1);
+	}
+	voir_touche();
+	return (0);
+}
+
+
