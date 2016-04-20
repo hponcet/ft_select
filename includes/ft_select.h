b8422825b42b7c9132cf267756d859aa01b1656e
Hugues PONCET <hponcet@e3r9p11.42.fr>
2016-04-20 18:23:09 +0200

    gitp

--- includes/ft_select.h
+++ includes/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: hponcet <hponcet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/18 20:05:35 by hponcet           #+#    #+#             */
-/*   Updated: 2016/04/18 21:34:39 by hponcet          ###   ########.fr       */
+/*   Updated: 2016/04/20 18:18:35 by hponcet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,32 @@
 # include <term.h>
 # include <stdlib.h>
 # include <curses.h>
+#include <sys/ioctl.h>
+#include <signal.h>
 # include "../libft/includes/libft.h"
 # define t_term struct termios
 
+void    ft_catch_wndw_mod(int signo);
 int     voir_touche(void);
 int     ft_shell_init(void);
 int     ft_shell_mod(t_term term);
 int		ft_popopo(int i);
+void    ft_clear_screen(void);
 
 #endif
+
+/*
+
+Pour deplacer le curseur les fonctions :
+tputs
+tgoto
+
+pour trouver la taille de la fenetre :
+ioctl
+
+pour intercepter les signaux :
+signal
+
+
+
+*/
--- main.c
+++ main.c
@@ -6,17 +6,19 @@
 /*   By: hponcet <hponcet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/15 15:02:50 by hponcet           #+#    #+#             */
-/*   Updated: 2016/04/18 21:43:10 by hponcet          ###   ########.fr       */
+/*   Updated: 2016/04/20 18:19:39 by hponcet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_select.h"
 
-int              main(void)
+int              main(int ac, char **av)
 {
 	char		*terminfo;
 	t_term		term;
 
+	ac = 0;
+	av = NULL;
 	if ((terminfo = getenv("TERM")) == NULL)
 		return (-1);
 	if (tgetent(NULL, terminfo) == ERR)
@@ -29,16 +31,32 @@ int              main(void)
 	return (0);
 }
 
+void	ft_catch_wndw_mod(int signo)
+{
+	ft_clear_screen();
+	if (signo == SIGWINCH)
+		ft_printf("La fenetre a ete modifiee\n");
+	// Le code pour afficher en collonne se trouvera ici grace a ioctl.
+}
+
+void	ft_clear_screen(void)
+{
+		tputs(tgetstr("cl", NULL), 1, ft_popopo);
+}
+
 int     voir_touche(void)
 {
 	char	buffer[5];
 
+	ft_clear_screen();
 	while (1)
 	{
 		read(0, buffer, 5);
 		if (buffer[0] == 27)
 			ft_printf("C'est une fleche !\n");
 		if (buffer[0] == 4)
+			return (0);
+		if (signal(SIGWINCH, ft_catch_wndw_mod) == SIG_ERR)
 			return (0);
 	}
 	return (0);
